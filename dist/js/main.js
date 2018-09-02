0e22f0dd2cca058f37ac19e2494a0fc6bfb3d6e2
Jose Guerra <jose@gcostudios.com>
2018-09-02 16:19:14 +0100

    updated file

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,36 +9,37 @@
 (function (window, document, undefined) {
   'use strict';
 
-  window.addEventListener("load", function() {
+  window.addEventListener('load', function() {
     // store tabs variable
-    var myTabs = document.querySelectorAll("ul.nav-tabs > li");
-
+    var myTabs = document.querySelectorAll('ul.nav-tabs > li'),
+        i = 0;
+    
     function myTabClicks(tabClickEvent) {
 
-      for (var i = 0; i < myTabs.length; i++) {
-        myTabs[i].classList.remove("active");
+      for (i = 0; i < myTabs.length; i++) {
+        myTabs[i].classList.remove('active');
       }
 
       var clickedTab = tabClickEvent.currentTarget;
 
-      clickedTab.classList.add("active");
+      clickedTab.classList.add('active');
       tabClickEvent.preventDefault();
 
-      var myContentPanes = document.querySelectorAll(".tab-pane");
+      var myContentPanes = document.querySelectorAll('.tab-pane');
 
       for (i = 0; i < myContentPanes.length; i++) {
-        myContentPanes[i].classList.remove("active");
+        myContentPanes[i].classList.remove('active');
       }
 
       var anchorReference = tabClickEvent.target;
-      var activePaneId = anchorReference.getAttribute("href");
+      var activePaneId = anchorReference.getAttribute('href');
       var activePane = document.querySelector(activePaneId);
-      
-      activePane.classList.add("active");
-    }
+
+      activePane.classList.add('active');
+    } // End of myTabClicks
 
     for (i = 0; i < myTabs.length; i++) {
-      myTabs[i].addEventListener("click", myTabClicks)
+      myTabs[i].addEventListener('click', myTabClicks);
     }
   });
 
